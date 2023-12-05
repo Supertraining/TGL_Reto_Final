53f196d0b9a615930dd56434f728ef850201c391
MatíasJMaranga <marangamatias@gmail.com>
2023-12-05 16:46:12 -0300

    roomsInitialStructure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,68 +1,9 @@
-import { useState } from 'react'
 import './App.css'
-import 'react-date-range/dist/styles.css'; // main style file
-import 'react-date-range/dist/theme/default.css'; // theme css file
-import { DateRange } from 'react-date-range';
-import { format } from 'date-fns';
-import axios from 'axios';
+
 import Home from './pages/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
-
-  const [ dates, setDates ] = useState([
-    {
-      startDate: new Date(),
-      endDate: new Date(),
-      key: 'selection',
-    },
-  ]);
-
-  const [ showDates, setShowDates ] = useState(false)
-
-  // console.log('DATES:', dates)
-
-  const getDatesInRange = (startDate, endDate) => {
-    const start = new Date(startDate);
-    const end = new Date(endDate);
-    const date = new Date(start.getTime());
-
-    let dates = [];
-
-    while (date <= end) {
-      dates.push(new Date(date).getTime());
-      date.setDate(date.getDate() + 1);
-    }
-    return dates;
-  };
-
-  const selectedDates = getDatesInRange(dates[ 0 ].startDate, dates[ 0 ].endDate);
-  // console.log('ALLDATES:', selectedDates)
-
-  const getRooms = async () => {
-    try {
-
-      const dbRooms = await axios.get('http://localhost:3000/api/room');
-
-      // console.log('ROOMS:', dbRooms.data)
-
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  const availableRooms = async () => {
-    try {
-
-      const rooms = await axios.post('http://localhost:3000/api/room/availableRooms', { selectedDates: selectedDates })
-
-      // console.log(rooms.data)
-
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
 
   return (
     <>
@@ -71,54 +12,6 @@ function App() {
           <Route path='/' element={ <Home /> } />
         </Routes>
       </BrowserRouter>
-
-
-      <div style={ { marginLeft: '50%', marginTop: '20%' } }>
-
-        <div>
-          <div>
-            IMAGE
-          </div>
-
-
-          <button onClick={ () => setShowDates(!showDates) }>
-            SINGLE ROOM
-          </button>
-
-        </div>
-
-        { showDates &&
-          <>
-            <div>
-              <h3>Seleccione una fecha</h3>
-              <DateRange
-                editableDateInputs={ true }
-                onChange={ (item) => setDates([ item.selection ]) }
-                moveRangeOnFirstSelection={ false }
-                ranges={ dates }
-                className='date'
-                minDate={ new Date() }
-              />
-            </div>
-
-            <div>
-              <span>{ `${format(
-                dates[ 0 ].startDate,
-                'MM/dd/yyyy'
-              )} to ${format(dates[ 0 ].endDate, 'MM/dd/yyyy')}` }
-              </span>
-            </div>
-
-
-            <button onClick={ () => availableRooms() }>
-              Habitaciones disponibles
-            </button>
-          </>
-        }
-
-
-      </div>
-
     </>
   )
 }
---
+++ client/src/components/rooms/Rooms.jsx
@@ -0,0 +1,116 @@
+import { useState } from 'react'
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
+import { DateRange } from 'react-date-range';
+import { format } from 'date-fns';
+import axios from 'axios';
+
+const Rooms = () => {
+
+  const [ dates, setDates ] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: 'selection',
+    },
+  ]);
+
+  const [ showDates, setShowDates ] = useState(false)
+
+  // console.log('DATES:', dates)
+
+  const getDatesInRange = (startDate, endDate) => {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+    const date = new Date(start.getTime());
+
+    let dates = [];
+
+    while (date <= end) {
+      dates.push(new Date(date).getTime());
+      date.setDate(date.getDate() + 1);
+    }
+    return dates;
+  };
+
+  const selectedDates = getDatesInRange(dates[ 0 ].startDate, dates[ 0 ].endDate);
+  // console.log('ALLDATES:', selectedDates)
+
+  // const getRooms = async () => {
+  //   try {
+
+  //     const dbRooms = await axios.get('http://localhost:3000/api/room');
+
+  //     return dbRooms.data
+
+  //   } catch (error) {
+  //     console.error(error)
+  //   }
+  // }
+
+  const availableRooms = async () => {
+    try {
+
+      const rooms = await axios.post('http://localhost:3000/api/room/availableRooms', { selectedDates: selectedDates })
+
+      return rooms.data
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+
+  return (
+    <div>
+      <div style={ { marginLeft: '50%', marginTop: '20%' } }>
+
+        <div>
+          <div>
+            IMAGE
+          </div>
+
+
+          <button onClick={ () => setShowDates(!showDates) }>
+            SINGLE ROOM
+          </button>
+
+        </div>
+
+        { showDates &&
+          <>
+            <div>
+              <h3>Seleccione una fecha</h3>
+              <DateRange
+                editableDateInputs={ true }
+                onChange={ (item) => setDates([ item.selection ]) }
+                moveRangeOnFirstSelection={ false }
+                ranges={ dates }
+                className='date'
+                minDate={ new Date() }
+              />
+            </div>
+
+            <div>
+              <span>{ `${format(
+                dates[ 0 ].startDate,
+                'MM/dd/yyyy'
+              )} to ${format(dates[ 0 ].endDate, 'MM/dd/yyyy')}` }
+              </span>
+            </div>
+
+
+            <button onClick={ () => availableRooms() }>
+              Habitaciones disponibles
+            </button>
+          </>
+        }
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Rooms
