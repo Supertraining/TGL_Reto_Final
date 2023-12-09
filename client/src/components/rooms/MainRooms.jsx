e6ad86ebcd0b07166b2c713b7609fe9d7a157009
Maria Clara <mariac.uribe1606@gmail.com>
2023-12-08 19:46:53 -0500

    Fixed styles on MainRoom component

--- client/src/components/rooms/MainRooms.jsx
+++ client/src/components/rooms/MainRooms.jsx
@@ -8,7 +8,6 @@ import { useNavigate } from 'react-router-dom';
 
 const MainRooms = () => {
 
-
   const navigate = useNavigate()
 
   const [ dates, setDates ] = useState([
@@ -18,7 +17,6 @@ const MainRooms = () => {
       key: 'selection',
     },
   ]);
-
 
   const getDatesInRange = (startDate, endDate) => {
     const start = new Date(startDate);
@@ -57,61 +55,30 @@ const MainRooms = () => {
     {
       "img": "../../public/simple.jpeg",
       "type": "Single",
-      "description": "Habitacion Simple",
+      "description": "One double bed available",
       "price": "USD 20",
     },
     {
       "img": "../../public/doble.jpeg",
       "type": "Double",
-      "description": "Habitacion doble",
+      "description": "Two single beds available",
       "price": "USD 35",
     },
     {
       "img": "../../public/quadruple.jpeg",
       "type": "Quadruple",
-      "description": "Habitacion cuadruple",
+      "description": "Four single beds available",
       "price": "USD 50",
     },
   ]
 
-
-
   return (
     <div className='mainRooms-container'>
 
-      <h1 className='titulo'>Nuestras habitaciones</h1>
-
-      <div id="carouselExampleCaptions" class="carousel slide" style={ { padding: '10%' } }>
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <div class="carousel-inner">
-
-          { habitaciones && habitaciones.map((habitacion) => (
-            <div class="carousel-item active">
-              <img src={ habitacion.img } class="d-block w-100" alt="..." />
-              <div class="carousel-caption d-none d-md-block">
-                <h5>{ habitacion.type }</h5>
-                <p>{ habitacion.description }</p>
-                <p>{ habitacion.price }</p>
-              </div>
-            </div>
-          )) }
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
+      <h1 className='titulo'>Our Rooms</h1>
       
       <div className='card-dates-container'>
-        <h3>Seleccione una fecha</h3>
+        <h3>Select a date</h3>
         <DateRange
           editableDateInputs={ true }
           onChange={ (item) => setDates([ item.selection ]) }
@@ -125,8 +92,8 @@ const MainRooms = () => {
       <div className='card-container'>
 
         { habitaciones && habitaciones.map((habitacion) => (
-          <div className='roomCard' key={ window.crypto.randomUUID() }>
-            <div class="card mb-3" style={ { width: '100%' } }>
+          <div className='roomCard' style={{ display: 'flex', alignItems: 'center'}} key={ window.crypto.randomUUID() }>
+            <div class="card mb-3" style={ { width: '50%' } }>
               <img src={ habitacion.img } class="card-img-top" alt="..." />
               <div class="card-body">
                 <h5 class="card-title">{ habitacion.type }</h5>
@@ -135,21 +102,18 @@ const MainRooms = () => {
               </div>
             </div>
 
-            <button
-              className='card-btn'
-              onClick={ () => availableRooms(habitacion.type) }>
-              Available rooms
-            </button>
+            <div style={{ padding: '30px' }}>
+              <button
+                className='card-btn'
+                style={{ height: '40px', border: 'none', background: '#0071c2', color: 'white', cursor: 'pointer', borderRadius: '5px', fontWeight: 'bold' }}
+                onClick={ () => availableRooms(habitacion.type) }>
+                Check Available Rooms
+              </button>
+            </div>
 
           </div>
-
-
-
         )) }
       </div>
-
-
-
     </div>
   )
 }
