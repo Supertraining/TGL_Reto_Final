00d3ee6bd63d2360c3b1ecad7aad6e22221a9056
Andrescor <anpipecordoba@gmail.com>
2023-12-08 23:12:24 -0500

    navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -97,14 +97,14 @@ const handleClick = async () => {
             <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
               Login
             </button>
-            <form class="dropdown-menu p-4">
+            <form class="dropdown-menu p-4" >
               <div class="mb-3">
                 <label for="exampleDropdownFormEmail2" class="form-label">User name</label>
-                <input type="email" class="form-control" id="exampleDropdownFormEmail2" placeholder="username" onChange={ handleChange }/>
+                <input type="email" class="form-control-lg" id="exampleDropdownFormEmail2" placeholder="username" onChange={ handleChange }/>
               </div>
               <div class="mb-3">
                 <label for="exampleDropdownFormPassword2" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Password" onChange={ handleChange } />
+                <input type="password" class="form-control-lg" id="exampleDropdownFormPassword2" placeholder="Password" onChange={ handleChange } />
               </div>
               <button disabled={ loading } class="btn btn-primary"onClick={ handleClick } type="submit" >Login</button>
             { error && <span>{ error.message }</span> }
